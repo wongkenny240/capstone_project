bb7c3ec6881be5458859f9f2133bc76177f21b9a
Remix Workspace <>
2022-10-16 15:28:33 +0800

    update first issue

---
+++ contracts/FirstContract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract PropertyContract
+{
+    struct Property{
+        address owernerAddress;
+        string location;
+        string unit_block;
+        string unit_floor;
+        string unit_flat;
+        uint cost;
+        uint prop_id;
+    }
+
+
+
+
+}
