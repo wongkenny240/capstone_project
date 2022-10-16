192f23d3a5719fcf47e28e8e4fdc69f509f4bb86
Remix Workspace <>
2022-10-16 17:57:58 +0800

    update first issue

--- contracts/FirstContract.sol
+++ contracts/FirstContract.sol
@@ -4,6 +4,10 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract PropertyContract
 {
+
+    enum Status {NotExist, Pending, Approved, Rejected}
+
+
     struct Property{
         address owernerAddress;
         string location;
@@ -14,7 +18,11 @@ contract PropertyContract
         uint prop_id;
     }
 
+    // initialised the contract
+    constructor() public {
 
+
+    }
 
 
 }
