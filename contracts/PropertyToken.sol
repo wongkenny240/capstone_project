ab70ee8841b1a4a56d35574c86fceea6b9596458
Kenny <ckwongkennyw@gmail.com>
2022-10-23 21:55:57 +0800

    update property token

--- contracts/PropertyToken.sol
+++ contracts/PropertyToken.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 import "@openzeppelin/contracts/drafts/Counters.sol";
 
-contract PropertyContract
+contract PropertyContract is ERC721Full
 {
 
     enum Status {NotExist, Pending, Approved, Rejected}
@@ -22,9 +22,14 @@ contract PropertyContract
     }
 
     // initialised the contract
-    constructor() public {
+    constructor(string memory _name, string memory _symbol) 
+        ERC721Full(_name, _symbol) public {
+    }
 
-
+    function registerContract(uint256 _tokenId, string memory _uri) public returns (){
+        _mint(msg.sender, _tokenId);
+        addContractMetadata(_tokenId, _uri);
+        emit ContractRegistered(msg.sender, _tokenId);
     }
 
 
