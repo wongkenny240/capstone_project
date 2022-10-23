4a5bfc4beee604edd03fbbd1a01a8c687ba7c9fd
Kenny <ckwongkennyw@gmail.com>
2022-10-23 21:49:09 +0800

    rename first contract to property token

---
+++ contracts/PropertyToken.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/drafts/Counters.sol";
+
+contract PropertyContract
+{
+
+    enum Status {NotExist, Pending, Approved, Rejected}
+
+
+    struct Property{
+        address owernerAddress;
+        string location;
+        string unit_block;
+        string unit_floor;
+        string unit_flat;
+        uint cost;
+        uint prop_id;
+    }
+
+    // initialised the contract
+    constructor() public {
+
+
+    }
+
+
+}
