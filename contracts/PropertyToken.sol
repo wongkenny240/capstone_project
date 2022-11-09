c75ed46e1a2ada92886631d5743f04e3eeac96ed
Kenny Wong <kenny.ck.wong@hk.pwc.com>
2022-11-09 16:35:58 +0800

    update property token

--- contracts/PropertyToken.sol
+++ contracts/PropertyToken.sol
@@ -10,22 +10,41 @@ contract PropertyContract is ERC721
 
     enum Status {NotExist, OnSale, Sold}
 
-    
+    address owner;
+    uint target_selling_price;
 
     struct Property{
-        address owernerAddress;
         string location;
         string unit_block;
         string unit_floor;
         string unit_flat;
-        uint cost;
-        uint prop_id;
+        string prop_id;
     }
 
     // initialised the contract
     constructor(string memory _name, string memory _symbol) 
         ERC721(_name, _symbol) public {
     }
+
+    function registerProperty(string memory _location, string memory _unit_block, string memory _unit_floor, string memory _unit_flat, string memory _prop_id) public {
+        //location = _location;
+        Property memory property = Property(
+            _location,
+            _unit_block,
+            _unit_floor,
+            _unit_flat,
+            _prop_id
+        );
+    }
+
+    function getPropertyDetail() public{
+        
+    }
+
+    function bidProperty(uint bidPrice) public{
+
+    }
+
 
     function registerContract(uint256 _tokenId, string memory _uri) public {
         _mint(msg.sender, _tokenId);
