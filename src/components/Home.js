d0828bb9614444ae6a17ba68f9d86ae8fe7e79b0
Kenny <ckwongkennyw@gmail.com>
2023-01-24 17:09:10 +0800

    update config

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+const Home = () => {
+    useEffect(() => {
+    }, []);
+    return (
+        <div><h2>Home</h2></div>
+    )
+}
+export default Home;
---
+++ src/components/NewBuilding.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+const NewBuilding = () => {
+useEffect(() => {
+}, []);
+return (
+<div><h2>Create a New Building</h2></div>
+)
+}
+export default NewBuilding;
