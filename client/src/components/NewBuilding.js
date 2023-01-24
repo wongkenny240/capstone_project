0557e29a5db1e011866e8f158a43b0f762c4bade
Kenny Wong <kenny.ck.wong@hk.pwc.com>
2023-01-25 01:09:22 +0800

    update

--- client/src/components/NewBuilding.js
+++ client/src/components/NewBuilding.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 const NewBuilding = () => {
-useEffect(() => {
-}, []);
-return (
-<div><h2>Create a New Building</h2></div>
-)
+    useEffect(() => {
+    }, []);
+    return (
+        <div><h2>Create a New Building</h2></div>
+    )
 }
 export default NewBuilding;
