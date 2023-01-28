fbee3b759e39f22d546e04e7633ed42c3ceb367f
Kenny Wong <kenny.ck.wong@hk.pwc.com>
2023-01-29 00:00:41 +0800

    update frontend

--- client/src/components/NewProperty.js
+++ client/src/components/NewProperty.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+import Web3 from "web3";
+import getWeb3 from "../getWeb3"
 
 const NewProperty = () => {
     useEffect(() => {
+        const init = async() => {
+
+
+
+
+        }
+
     }, []);
     return (
         <div className="form-submit"><h2>Create a New Building</h2>
